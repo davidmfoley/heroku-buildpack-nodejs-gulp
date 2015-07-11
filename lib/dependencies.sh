2e318192d55c01d5f8dd13b87f3491b00aa518c5
dh94 <halleldaniel@gmail.com>
2015-07-11 23:22:58 +0300

    add gulp, bower

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -33,3 +33,35 @@ rebuild_node_modules() {
     echo "Skipping (no package.json)"
   fi
 }
+
+install_bower_modules() {
+  local build_dir=${1:-}
+
+  if [ -e $build_dir/bower.json ]; then
+    # Install bower locally
+    npm install bower
+    echo "-----> Found bower.json, running bower install"
+    $build_dir/node_modules/.bin/bower install
+  else
+    echo "-----> Skipping (no bower.json)"
+  fi
+}
+
+run_gulp_script() {
+  local build_dir=${1:-}
+
+  if [ -e $build_dir/gulpfile.js ] || [ -e $build_dir/gulpfile.coffee ]; then
+    # get the env vars
+    if [ -d "$env_dir" ]; then
+      status "Exporting config vars to environment"
+      export_env_dir $env_dir
+    fi
+
+    # Install gulp locally
+    npm install gulp
+    echo "-----> Found gulpfile, running gulp heroku:$NODE_ENV task"
+    $build_dir/node_modules/.bin/gulp heroku:$NODE_ENV
+  else
+    echo "-----> No gulpfile found"
+  fi
+}
